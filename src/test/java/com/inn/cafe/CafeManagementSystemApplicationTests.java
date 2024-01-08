de4c4f441b1775dcd20645f4f7695bb7eb70b067
lenguyenquocdat1219@gmail.com <lenguyenquocdat1219@gmail.com>
2024-01-08 13:29:18 +0700

    add test

--- src/test/java/com/inn/cafe/CafeManagementSystemApplicationTests.java
+++ src/test/java/com/inn/cafe/CafeManagementSystemApplicationTests.java
@@ -1,13 +1,22 @@
 package com.inn.cafe;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class CafeManagementSystemApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext, "Application context should not be null");
+		// Add more assertions or checks related to your application context if needed
 	}
 
 }
